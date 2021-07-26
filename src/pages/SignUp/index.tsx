e2184dc22cb9358388bee79108d0a2665d42c9b0
Brendon <brendon3007gabriel@gmail.com>
2021-07-26 07:38:20 -0300

    Salvado

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,60 @@
+import React, { useRef, useCallback } from "react";
+import { FiArrowLeft, FiUser, FiLock } from "react-icons/fi";
+import { Form } from "@unform/web";
+import { FormHandles } from "@unform/core";
+import * as Yup from 'yup';
+
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+import { Container, Content, Background } from "./styles";
+import getValidationsErrors from "../../utils/getValidationErros";
+
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async(data: object) => {
+    try{
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+            email: Yup.string().required('E-mail obrigatório').email('Informe um e-mail válido'),
+            senha: Yup.string().required('Senha obrigatória').min(6, 'No mínimo 6 caracteres'),
+        });
+
+        await schema.validate(data, {abortEarly: false,
+        });
+    }catch(err){
+        const errors = getValidationsErrors(err);
+        formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input icon={FiUser} name="email" placeholder="E-mail" />
+          <Input
+            icon={FiLock}
+            name="senha"
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="teste">
+          <FiArrowLeft />
+          Voltar para o login
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import { Container, Background, Content } from './style';
+import React from "react";
+import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
 
-import Input from '../../components/input';
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 
-import { FiLogIn } from 'react-icons/fi'
+import { Container, Content, Background } from "./styles";
 
 const SignIn: React.FC = () => (
-    <Container>
-        <Content>
-            <form>
-                <h1>Faça seu login</h1>
+  <Container>
+    <Content>
+      <form>
+        <h1>Faça seu login</h1>
 
-                <Input name="Email" />
-                <Input type="password" name="Senha" />
+        <Input icon={FiMail} name="email" placeholder="E-mail" />
+        <Input icon={FiLock} name="senha" type="password" placeholder="Senha" />
 
-                <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
-                <a href="teste">Esqueci minha senha</a>
-            </form>
-            <a href="teste">
-                <FiLogIn/>
-                Criar conta
-            </a>
-        </Content>
-        <Background/>
-    </Container>
+        <a href="teste">Esqueci minha senha</a>
+      </form>
 
-
+      <a href="teste">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
 );
 
 export default SignIn;
